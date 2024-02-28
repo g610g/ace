86fa7823d0d7c60e7c5eab94d40dab069f68d1d4
g610g <gio.gonzales@carsu.edu.ph>
2024-02-28 14:22:33 +0800

    fixed issue for appending might change in the future for efficiency problem

--- src/rope/mod.rs
+++ src/rope/mod.rs
@@ -66,10 +66,14 @@ impl Rope{
         }
     }
     fn get_weight(&self) -> usize{
-        if let WeightNode(w) = self{
-            return w.weight;
+        match self{
+            WeightNode(w) => {
+                return w.weight;
+            },
+            LeafNode(l) => {
+                return l.length;
+            }
         }
-        return 0;
     }
     fn return_weight_struct(self) -> WeightNode{
         match self {
@@ -93,43 +97,51 @@ impl Rope{
         )
     }
     pub fn new(string: &str) -> Result<Rope, &str>{
-        let leaf_node = Self::new_leaf_node(string);
-        if leaf_node.is_some(){
-            //creates the struct weightNode as mutable
-            let mut w = WeightNode::new(string.len(), 1);
-            //assign the weight_node to the newly leaf_node
-            w.left = leaf_node;
-            //wrap enum weight_node the struct weight_node
-            let weight_node = WeightNode(w);
-            return Ok(weight_node);
-        }
-        Err("error creating rope")
+        // let leaf_node = Self::new_leaf_node(string);
+        let leaf_node = LeafNode::new(string);
+        Ok(LeafNode(leaf_node))
     }
     pub fn append(self, string: &str)-> Result<Rope, &str>{
+        //takes O(1) without balancing
         match self{
-            WeightNode(mut w) => {
+            WeightNode(w) => {
+                //creates the new leafnode for the string
                 let new_ln = Self::new_leaf_node(string);
-                w.right = new_ln;
-                let mut new_w = WeightNode::new(w.weight + string.len(), w.height + 1);
-               new_w.left =  WeightNode(w).balance();
-                //rebalance should be made here
-                //also change the weight of the new_w since after balancing weights will be adjusted
-                return Ok(WeightNode(new_w));
+                let mut left_weight:usize = 0;
+                left_weight += w.weight;
+                if let Some(right) = w.right.as_ref(){
+                    left_weight += right.get_weight();
+                }
+                println!("Total left weight:{left_weight}");
+                let mut new_root = WeightNode::new(left_weight, 0);
+                //new root right is the new leaf
+                new_root.right = new_ln;
+                //new root left is the current rope the we made
+                //find the height difference of your left side and then balance it 
+                new_root.left = Self::new_weight_node(w);
+                
+                return Ok(WeightNode(new_root));
+            },
+            LeafNode(l) => {
+                let mut weight_root = WeightNode::new(l.length, 0);
+                weight_root.left = Self::new_leaf_node(&l.string);
+                weight_root.right = Self::new_leaf_node(string);
+                let root = WeightNode(weight_root);
+                return Ok(root);
             }
-            _ => {Err("Error appending a string")}
         }
     }
-    pub fn get_height(&mut self) -> i32{
+    pub fn get_height(&self) -> i32{
         if self.is_leaf(){
             return 0;
         }
         if let WeightNode(w)  = self{
             let mut  left_height = 0;
             let mut right_height = 0;
-            if let Some(left) = w.left.as_mut(){
+            if let Some(left) = w.left.as_ref(){
                 left_height = left.get_height();
             }
-            if let Some(right) = w.right.as_mut(){
+            if let Some(right) = w.right.as_ref(){
                 right_height = right.get_height();
             }
             return left_height.max(right_height) + 1;
@@ -156,29 +168,48 @@ impl Rope{
         }
 
     }
-    
-       fn balance(self) -> Option<Box<Rope>>{
+    fn get_height_difference(&self) -> i32{
+        if self.is_leaf(){
+            return 0;
+        }
+        let mut left_height = 0;
+        let mut right_height = 0;
+        if let WeightNode(w) = self{
+            if let Some(w_left) = w.left.as_ref(){
+                left_height = w_left.get_height();
+            }
+            if let Some(w_right) = w.right.as_ref(){
+                right_height = w_right.get_height();
+            }
+        }
+        return left_height - right_height;
+    }
+       fn balance(self) -> Result<Rope, &'static str>{
         if self.is_leaf(){
             panic!("Cannot balance or rotate leaf node!");
         }
+        //get height difference of our left side. If it is zero or greater then do not rotate your left. If it is -1 or less than then rotate it to the left
+        
         match self{
             WeightNode(mut w) => {
-                let mut left_height = 0;
-                let mut right_height = 0;
-                if let Some(left) = w.left.as_mut(){
-                   left_height = left.get_height();
-
+                if let Some(mut left) = w.left{
+                    //left rotate
+                    if left.get_height_difference() <= -1 {
+                        w.left = left.return_weight_struct().rotate_left();
+                    }
                 }
-                if let Some(right) = w.right.as_mut(){
-                    right_height = right.get_height();
-                }
-                if left_height - right_height > 1 || left_height - right_height < -1{
+                //box will be returned instead of rope yawa!
+                return w.rotate_right().unwrap();
+                // if let Some(right) = w.right.as_ref(){
+                //     right_height = right.get_height();
+                // }
+                // if left_height - right_height > 1 || left_height - right_height < -1{
                                     
-                    //you have to find a way to extract the weight struct and rotate it to the left and will return a Rope num
-                    return w.rotate_right();
-                }else{
-                    return Rope::new_weight_node(w);
-                }
+                //     //you have to find a way to extract the weight struct and rotate it to the left and will return a Rope num
+                //     return w.rotate_right();
+                // }else{
+                //     return Rope::new_weight_node(w);
+                // }
                 
             },
             _ => {panic!("Leaf node ni")}
