6c0e353fa7549fa8d0cf8b79080d78671f0c0a4b
g610g <gio.gonzales@carsu.edu.ph>
2024-03-22 12:47:31 +0800

    we are back bois

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-extern  crate sdl2;
-
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use std::time::Duration;
+use sdl2::rect::Rect;
+use std::{path::Path, time::Duration};
 use ace::rope::{*};
 fn main() -> Result<(), String>{
+    // pkg_config::probe_library
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    let keyboard_util = sdl_context.keyboard();
 
     let window = video_subsystem
                 .window("Ace", 800, 600)
@@ -15,11 +16,12 @@ fn main() -> Result<(), String>{
                 .opengl()
                 .build()
                 .map_err(|e| e.to_string())?;
+ 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-    let text_input = video_subsystem.text_input().start();
-    canvas.set_draw_color(Color::RGB(128, 128, 128));
-    canvas.clear();
-    canvas.present();
+    // canvas.set_draw_color(Color::RGB(128, 128, 128));
+    // canvas.clear();
+    // canvas.present();
+    
     let mut event_pump = sdl_context.event_pump()?;
     
     'running: loop {
@@ -30,18 +32,27 @@ fn main() -> Result<(), String>{
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::TextInput { timestamp: (time), window_id: (id), text: (string_input) } => {
+
+                },
                 _ => {}
             }
         }
 
         canvas.clear();
+        let font_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
+        let font_path = Path::new("asset/fonts/Raleway-VariableFont_wght.ttf");
+        let mut raleway_font = font_context.load_font(font_path, 24).unwrap();
+        let surface = raleway_font.render("Hello world").blended(Color::RGB(255,160,122)).unwrap();
+        let texture_creator = canvas.texture_creator();
+        let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
+        let texture_query = texture.query();
+        let texture_rec = Rect::new(0, 0, texture_query.width, texture_query.height);
+        canvas.copy(&texture, None,  texture_rec).unwrap();
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
         // The rest of the game loop goes here...
+
     }
-    // let path = "asset/test.txt";
-    // let mut rope = Rope::from_file(path).unwrap();
-    // rope = rope.append("string").unwrap();
-    // println!("{:?}", rope);
     return Ok(());
 }
